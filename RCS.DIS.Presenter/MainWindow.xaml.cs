864752574c6bc21ff2a3455c62efbd902c5fe8d1
a-einstein <aeinstein@xs4all.nl>
2018-02-19 15:12:04 +0100

    Initial application with Diagnose. Exception, probably because of serialization.

--- RCS.DIS.Presenter/MainWindow.xaml.cs
+++ RCS.DIS.Presenter/MainWindow.xaml.cs
@@ -1,28 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Prism.Commands;
+using RCS.DIS.Presenter.RetrieveService.ServiceReference;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RCS.DIS.Presenter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            // TODO add enablement.
+            SearchCommand = new DelegateCommand(Search);
+        }
+
+        public static readonly DependencyProperty SearchStringProperty =
+            DependencyProperty.Register("SearchString", typeof(string), typeof(MainWindow));
+
+        public string SearchString
+        {
+            get { return (string)GetValue(SearchStringProperty); }
+            set { SetValue(SearchStringProperty, value); }
+        }
+
+        public static readonly DependencyProperty SearchCommandProperty =
+            DependencyProperty.Register("SearchCommand", typeof(ICommand), typeof(MainWindow));
+
+        public ICommand SearchCommand
+        {
+            get { return (ICommand)GetValue(SearchCommandProperty); }
+            set { SetValue(SearchCommandProperty, value); }
+        }
+
+        public void Search()
+        {
+            var number = new RetrieveServiceClient().DiagnoseOmschrijvingContainsNumber(SearchString);
+
+            if (number > 100)
+                Message = $"Found {number}. Please refine your query.";
+            else
+            {
+                Message = $"Found {number}. Please select a diagnose.";
+                Diagnoses = new RetrieveServiceClient().DiagnoseOmschrijvingContainsEntities(SearchString);
+            }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(MainWindow), new PropertyMetadata("Enter part of the omschrijving to look for."));
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty DiagnosesProperty =
+            DependencyProperty.Register("Diagnoses", typeof(Diagnose[]), typeof(MainWindow), new PropertyMetadata(new Diagnose[0]));
+
+        public Diagnose[] Diagnoses
+        {
+            get { return (Diagnose[])GetValue(DiagnosesProperty); }
+            set { SetValue(DiagnosesProperty, value); }
         }
     }
 }
